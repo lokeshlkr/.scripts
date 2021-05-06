da247b3a51163ab3a3bc38d3ad6302bc66c7d91f
Lokesh Kumar <lokeshlkr@gmail.com>
2021-05-07 04:06:06 +0530

    syncing

---
+++ shortcut-helper/shelp.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+args=($@)
+rest="${args[@]:1}"
+
+if [[ $1 = "scripts" ]] ; then
+    nvim ~/.scripts/
+    exit
+fi
+
+if [[ $1 = "updatetheme" ]] ; then
+    nvim ~/.config/xfce4/terminal/terminalrc
+    exit
+fi
+
+if [[ $1 = "search" ]] ; then
+    firefox "https://duckduckgo.com/?q=$rest"
+    exit
+fi
+
+if [[ $1 = "push" ]] ; then
+    if [[ -d ./.git ]] ; then
+        git add . && git commit -m "syncing" && git push origin master || echo Something went wrong.
+    else
+        echo Not a git repository.
+    fi
+    exit
+fi
