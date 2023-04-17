b1f4500eb755368c70870445a86920d9c2da6e69
Lokesh Kumar <lokeshlkr@gmail.com>
2023-04-18 00:31:04 +0530

    autosync:

--- autostart/setup_new_install.sh
+++ autostart/setup_new_install.sh
@@ -1,9 +1,18 @@
 #! /usr/bin/sh
 
+# only for arch-based xfce distro
+
+# install stranger nerd fonts first
+
 sudo pacman -Syyu
 sudo pacman -S redshift sxhkd xfce4-genmon-plugin rofi alacritty paru
 paru -S vscodium
+
 mkdir ~/.config
 mkdir ~/.config/sxhkd
 ln ~/working_folder/.scripts/configs/sxhkdrc ~/.config/sxhkd/sxhkdrc
+
+mkdir ~/.config/alacritty
+ln ~/working_folder/.scripts/configs/alacritty.yml ~/.config/alacritty/alacritty.yml
+
 sudo ln ~/working_folder/.scripts/shortcut-helper/shelp.py /usr/bin/s
