63d328e123d1b85d2641d502e5425a8cf0763426
Lokesh Kumar <lokeshlkr@gmail.com>
2023-06-24 18:37:23 +0530

    added fonts

--- autostart/setup_new_install.sh
+++ autostart/setup_new_install.sh
@@ -4,11 +4,11 @@
 
 # install stranger nerd fonts first
 
-sudo pacman -Syy
-sudo pacman -S archlinux-keyring
-sudo pacman -Syu
-sudo pacman -S paru 
-paru -S redshift 
+sudo pacman -Syy                 # update all repositories
+sudo pacman -S archlinux-keyring # update arch-linux keyring
+sudo pacman -Syu                 # update whole system
+sudo pacman -S paru              # install paru (AUR helper)
+paru -S redshift
 paru -S sxhkd 
 paru -S xfce4-genmon-plugin 
 paru -S rofi 
@@ -25,23 +25,26 @@ echo "" >> ~/.bashrc
 echo "source ~/working_folder/.scripts/configs/bash_extras.sh" >> ~/.bashrc
 source ~/.bashrc
 
-sudo cp ~/working_folder/.scripts/fonts/StrangerMonoNF/* /usr/share/fonts/
-sudo cp ~/working_folder/.scripts/fonts/StrangerSansNF/* /usr/share/fonts/
+sudo cp ~/working_folder/.scripts/fonts/StrangerMono/* /usr/share/fonts/
+sudo cp ~/working_folder/.scripts/fonts/StrangerSans/* /usr/share/fonts/
 sudo cp ~/working_folder/.scripts/fonts/GoogleSans/* /usr/share/fonts/
 
 mkdir -p ~/.config/sxhkd
-ln ~/working_folder/.scripts/configs/sxhkdrc ~/.config/sxhkd/sxhkdrc
+ln -s ~/working_folder/.scripts/configs/sxhkdrc ~/.config/sxhkd/sxhkdrc
 
 mkdir -p ~/.config/alacritty
-ln ~/working_folder/.scripts/configs/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s ~/working_folder/.scripts/configs/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+mkdir -p ~/.config/helix
+ln -s ~/working_folder/.scripts/configs/helix.toml ~/.config/helix/config.toml
 
 mkdir -p ~/.config/rofi
-ln ~/working_folder/.scripts/configs/config.rasi ~/.config/rofi/config.rasi
-ln ~/working_folder/.scripts/configs/rofi-theme.rasi ~/.config/rofi/rofi-theme.rasi
+ln -s ~/working_folder/.scripts/configs/config.rasi ~/.config/rofi/config.rasi
+ln -s ~/working_folder/.scripts/configs/rofi-theme.rasi ~/.config/rofi/rofi-theme.rasi
 
 mkdir -p ~/.themes
 mkdir -p ~/.icons
 cp ~/working_folder/.scripts/xfce/.themes/* ~/.themes
 cp ~/working_folder/.scripts/xfce/.icons/* ~/.icons
 
-sudo ln ~/working_folder/.scripts/shortcut-helper/shelp.py /usr/bin/s
+sudo ln -s ~/working_folder/.scripts/shortcut-helper/shelp.py /usr/bin/s
--- configs/bash_extras.sh
+++ configs/bash_extras.sh
@@ -3,7 +3,7 @@
 
 CM_LAUNCHER=rofi
 
-alias ls='ls --color=auto'
+alias ls='ls -lah --color=auto --ignore=..'
 alias ll='ls -lav --ignore=..'   # show long listing of all except ".."
 alias l='ls -lav --ignore=.?*'   # show long listing but no hidden dotfiles except "."
 alias lf='ls | grep -i'
@@ -14,6 +14,7 @@ alias remove='paru -Rns'
 alias update='paru -Syyu'
 alias exe='chmod +x'
 alias code='codium'
+alias hx='helix'
 
 alias 'cd..'='cd ..'
 
@@ -23,3 +24,4 @@ function mkcd(){
 }
 export -f mkcd
 
+PS1='\[\e[93m\]╭\[\e[92m\]\@ \[\e[93;1m\]◦ \[\e[22m\]\u \[\e[1m\]◦ \[\e[0;95m\]\w\[\e[38;5;242m\]⏎\n\[\e[93m\]╰─────────⌲ \[\e[0m\]'
