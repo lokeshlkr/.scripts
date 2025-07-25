a24c3290ab881839ec37f845d3a8a0119ff536a9
Lokesh Kumar <lokeshlkr@gmail.com>
2025-07-25 20:01:23 +0530

    adding fonts

---
+++ windows/references/installs.md
@@ -0,0 +1,6 @@
+# Install scoop:
+- `Set-ExecutionPolicy RemoteSigned -Scope CurrentUser`
+- `irm get.scoop.sh | iex`
+
+# Install zenity:
+-  `scoop install https://ncruces.github.io/scoop/zenity.json`
---
+++ windows/shelp/shelp.py
@@ -0,0 +1,226 @@
+#! /usr/bin/python
+
+# SHORTCUT HELPER #
+
+## python -m nuitka --standalone --onefile --disable-console --remove-output .\shelp.py
+
+import sys,os,ast,subprocess
+import datetime
+from plyer import notification
+from win10toast import ToastNotifier
+from time import sleep
+from enum import Enum
+#################### SETUP #####################
+
+class Level(Enum):
+    TERMINAL = 0
+    NOTIFICATION = 1
+    BOTH = 2
+
+colors = {
+    "style" : {
+        'reset' : '\033[0m',
+        'bold' : '\033[01m',
+        'disable' : '\033[02m',
+        'underline' : '\033[04m',
+        'reverse' : '\033[07m',
+        'strikethrough' : '\033[09m',
+        'invisible' : '\033[08m',
+    },
+    "fg" : {
+        'black' : '\033[30m',
+        'red' : '\033[31m',
+        'green' : '\033[32m',
+        'orange' : '\033[33m',
+        'blue' : '\033[34m',
+        'purple' : '\033[35m',
+        'cyan' : '\033[36m',
+        'lightgrey' : '\033[37m',
+        'darkgrey' : '\033[90m',
+        'lightred' : '\033[91m',
+        'lightgreen' : '\033[92m',
+        'yellow' : '\033[93m',
+        'lightblue' : '\033[94m',
+        'pink' : '\033[95m',
+        'lightcyan' : '\033[96m',
+    },
+    "bg" : {
+        'black' : '\033[40m',
+        'red' : '\033[41m',
+        'green' : '\033[42m',
+        'orange' : '\033[43m',
+        'blue' : '\033[44m',
+        'purple' : '\033[45m',
+        'cyan' : '\033[46m',
+        'lightgrey' : '\033[47m',
+    }
+}
+editor = "codium"
+browser = "firefox"
+terminal = "wt"
+home = os.path.expanduser("~")
+paths = {
+    'home' : home,
+    'rust' : f'{home}\\working_folder\\rust\\practice',
+    'self' : f'{home}\\working_folder\\.scripts\\windows',
+    ''     : f'{home}\\working_folder\\.scripts\\windows',
+}
+mappings = {
+    't':terminal,
+    'terminal':terminal,
+    'b':browser,
+    'browser':browser,
+}
+
+filename = sys.argv[0]
+command = rest = ""
+
+
+def run(command):
+    return os.system(command) == 0
+
+def zenity(command):
+    x = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    (stdout,error) = x.communicate()
+    res = str(stdout)[2:-5].strip() # slicing to get rid of quotes and new line character
+    print(res)
+    return res
+
+def is_git_repo(path):
+    if path == '.':
+        path = os.getcwd()
+    cwd = path.split('/')[1:]
+    isGitRepo = False
+    l = len(cwd)
+    for i in range(l):
+        path = '/' + '/'.join(cwd[:l-i])
+        dirs = os.listdir(path)
+        if '.git' in dirs:
+            return path
+
+def search():
+    command = f'{browser} "https://duckduckgo.com/?q={rest}" 2>/dev/null &'
+    run(command)
+
+def browse():
+    url = 'https://' + (rest if "." in rest else rest + ".com")
+    command = f'{browser} "{url}" 2>/dev/null &'
+    run(command) 
+
+def sync():
+    path = paths.get(rest,rest)
+    path = os.path.normpath(path)
+    if not os.path.exists(path):
+        return
+    git_path = is_git_repo(path)
+    if git_path != None:
+        os.chdir(git_path)
+        command = f'git add --all && git commit -m "autosync" && git push origin master'
+        run(command)
+
+def panel():
+    file = f'{home}/working_folder/.scripts/panel/{rest}'
+    run(file)
+
+def openineditor(path):
+    if os.path.exists(path):
+        run(f'{editor} {path}')
+        return True
+    return False
+
+def rustnew():
+    os.chdir(f'{paths["rust"]}/..')
+    run('mv practice "practice_$(date +%Y%m%d_%H%M%S)"')
+    run('cargo new practice')
+    openineditor(paths['rust'])
+
+def rust():
+    if not openineditor(paths['rust']):
+        rustnew()
+
+def edit():
+    path = paths.get(rest.strip(),None)
+    if path:
+        openineditor(path)
+
+def restart():
+    if len(rest.strip()) == 0:
+        return
+    else:
+        r=0
+        x = run(f'killall {rest}')
+        if(not x):
+            r = run(f'{rest} &')
+        else:
+            sleep(0.5)
+            r = run(f'{rest} &')
+
+def _resize(path,size, inplace=False):
+    if not os.path.isfile(path):
+        return False
+    ext = path.split(".")[-1]
+    oldname = f'{path[:-(len(ext)+1)]}_{datetime.datetime.now().strftime("%Y%m%dT%H%M%S")}.{ext}'
+    newname = path
+    if not inplace:
+        if not run(f'cp {path} {oldname}'):
+            newname += size
+    if run(f'convert {path} -resize {size} {newname}'):
+        return True
+    else:
+        return False
+
+
+def iresize():
+    paths = [os.path.normpath(path) for path in sys.argv[2:] if path.strip()]
+    size = zenity("zenity --title='Shelp' --entry --text='Enter new size as widthxheight:'")
+    success = 0
+    for path in paths:
+        if os.path.isfile(path) and not os.path.islink(path):
+            if _resize(path, size):
+                success += 1
+
+def iresize_inplace():
+    paths = [os.path.normpath(path) for path in sys.argv[2:] if path.strip()]
+    size = zenity("zenity --title='Shelp' --entry --text='Enter new size as widthxheight:'")
+    success = 0
+    for path in paths:
+        if os.path.isfile(path) and not os.path.islink(path):
+            if _resize(path, size, inplace=True):
+                success += 1
+
+def doit(command):
+    knownCommand = globals().get(command, None)
+    knownMapping = mappings.get(command,None)
+    if knownCommand:
+        knownCommand()
+    elif knownMapping:
+        run(knownMapping)
+
+def gui():
+    global rest
+    if run('taskkill /IM zenity /F'):
+        return
+    output = zenity('zenity --title="Shelp" --entry --text="Enter a shelp command:"')
+    output = output.split(" ")
+    command = output[0]
+    rest = " ".join(output[1:])
+    if command: doit(command)
+
+
+def testing():
+    run(f'echo {paths}')
+################################################
+#################### INIT ######################
+################################################
+
+# TODO
+# Instead of making `rest` a string with spaces
+# leave it as a list
+if __name__ == '__main__':
+    if(len(sys.argv) > 1):
+        command = sys.argv[1]
+        rest = " ".join(sys.argv[2:])
+        doit(command)
+################################################
+################################################
+################################################
